6197884ee3553bd7be84456955bdd4395f2e10c7
graf-style <utpplatano@gmail.com>
2023-04-18 15:22:49 -0400

    testing done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,9 +3,16 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-
 export class AppComponent {
   title = 'Actuality';
+}
+
+class cosas {
+  hoja: string = 'Hola mundo';
+
+  constructor() {
+    console.log(hoja);
+  }
 }
