7001037c70589fed24079b62cacedd57a86a3823
Osmaldy Maldonado <osmaldy11checo@gmail.com>
2023-04-18 20:05:42 -0400

    Adding all posts missing methods.

--- src/app/controller/posts/posts.ts
+++ src/app/controller/posts/posts.ts
@@ -1,1 +1,51 @@
-export class Posts {}
+import { Service } from '../services/services';
+
+export class Posts {
+  private path: string = 'posts';
+  constructor(private _apiService: Service) {}
+
+  get(): Promise<Post[]> {
+    return new Promise((res, rej) => {
+      this._apiService.get(this.path).subscribe({
+        next: (e: any) => res(e as Post[]),
+        error: () => rej([]),
+      });
+    });
+  }
+
+  post(data: Post): Promise<string> {
+    return new Promise((res, rej) => {
+      this._apiService.post(this.path, data).subscribe({
+        next: () => res('The data has been posted.'),
+        error: () => rej('Something went wrong when post the data.'),
+      });
+    });
+  }
+
+  put(id: number, data: Post): Promise<string> {
+    return new Promise((res, rej) => {
+      this._apiService.put(this.path, id, data).subscribe({
+        next: () => res('The data has been updated.'),
+        error: () => rej('Something went wrong when update the data.'),
+      });
+    });
+  }
+
+  delete(id: number): Promise<string> {
+    return new Promise((res, rej) => {
+      this._apiService.delete(this.path, id).subscribe({
+        next: () => res('The data has been deleted.'),
+        error: () => rej('Something went wrong when delete the data.'),
+      });
+    });
+  }
+}
+
+export interface Post {
+  id: number;
+  id_user: number;
+  body: string;
+  date_adding: string;
+  date_modified: string;
+  cant_likes: number;
+}
