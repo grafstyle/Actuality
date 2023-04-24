25dd6aba17e6f1b37ba69c72e6ae551e95ab19d5
Osmaldy Maldonado <osmaldy11checo@gmail.com>
2023-04-24 12:12:28 -0400

    Adding constructor with services

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,18 @@
 import { Component } from '@angular/core';
+import { Users } from './controller/users/users';
+import { AuthService } from '@auth0/auth0-angular';
+import { Service } from './controller/services/services';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-
 export class AppComponent {
   title = 'Actuality';
+
+  constructor(private auth: AuthService, private apiService: Service) {
+    Users.apiService = this.apiService;
+    Users.auth = this.auth;
+  }
 }
