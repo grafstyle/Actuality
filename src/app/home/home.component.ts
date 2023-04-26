866dc0f458c95499fb50eca9f313ba6c8cfcc233
Osmaldy Maldonado <osmaldy11checo@gmail.com>
2023-04-25 23:22:45 -0400

    Changing instances to static methods

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -3,6 +3,7 @@ import { Post, Posts } from '../controller/posts/posts';
 import { Component } from '@angular/core';
 import { User, Users } from '../controller/users/users';
 import { Comments, Comment } from '../controller/comments/comments';
+import { async } from 'rxjs';
 
 @Component({
   selector: 'app-home',
@@ -16,11 +17,9 @@ export class HomeComponent {
   comments: Comment[] = [];
   usersComments: User[] = [];
 
-  constructor(private _apiService: Service) {}
-
   async ngOnInit() {
     try {
-      this.posts = await new Posts(this._apiService).get();
+      this.posts = await Posts.getAll();
       await this.getAll();
     } catch (e) {
       this.err = 'Something went wrong when get the data.';
@@ -28,17 +27,17 @@ export class HomeComponent {
   }
 
   async getAll() {
-    const allComments: Comment[] = await new Comments(this._apiService).get();
+    const comments = await Comments.getAll();
 
     this.posts.forEach(async (post) => {
       this.users.push(await Users.get(post.id_user));
 
-      allComments.forEach((comment: Comment) => {
+      comments.forEach((comment: Comment) => {
         if (post.id == comment.id_post) this.comments.push(comment);
       });
     });
 
-    allComments.forEach(async (comment: Comment) => {
+    comments.forEach(async (comment: Comment) => {
       this.usersComments.push(await Users.get(comment.id_user));
     });
   }
