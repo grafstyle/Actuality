5b8ce4b90075011857df87c9bf625d0658431431
Osmaldy Maldonado <osmaldy11checo@gmail.com>
2023-04-25 23:22:45 -0400

    Changing instances to static methods

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -16,11 +16,9 @@ export class HomeComponent {
   comments: Comment[] = [];
   usersComments: User[] = [];
 
-  constructor(private _apiService: Service) {}
-
   async ngOnInit() {
     try {
-      this.posts = await new Posts(this._apiService).get();
+      this.posts = await Posts.getAll();
       await this.getAll();
     } catch (e) {
       this.err = 'Something went wrong when get the data.';
@@ -28,18 +26,12 @@ export class HomeComponent {
   }
 
   async getAll() {
-    const allComments: Comment[] = await new Comments(this._apiService).get();
-
     this.posts.forEach(async (post) => {
       this.users.push(await Users.get(post.id_user));
 
-      allComments.forEach((comment: Comment) => {
+      (await Comments.getAll()).forEach((comment: Comment) => {
         if (post.id == comment.id_post) this.comments.push(comment);
       });
-    });
-
-    allComments.forEach(async (comment: Comment) => {
-      this.usersComments.push(await Users.get(comment.id_user));
     });
   }
 }
