4edefdb2518708a561afcb1fa0f7d338d75ee412
Osmaldy Maldonado <osmaldy11checo@gmail.com>
2023-04-29 16:19:18 -0400

    Creating new component postInput

---
+++ src/app/post-input/post-input.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-post-input',
+  templateUrl: './post-input.component.html',
+  styleUrls: ['./post-input.component.css'],
+})
+export class PostInputComponent {}
