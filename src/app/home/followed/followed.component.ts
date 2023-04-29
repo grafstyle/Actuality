d0d3343b941231e1bdaf882638fc47b5b3f867c2
Osmaldy Maldonado <osmaldy11checo@gmail.com>
2023-04-29 19:32:30 -0400

    Creating subcomponent followed

---
+++ src/app/home/followed/followed.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-followed',
+  templateUrl: './followed.component.html',
+  styleUrls: ['./followed.component.css'],
+})
+export class FollowedComponent {}
