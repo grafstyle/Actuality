f7c0284e432031ee468872c56541b8dc64d162ba
Osmaldy Maldonado <osmaldy11checo@gmail.com>
2023-05-02 11:59:17 -0400

    Renaming component followed of home

--- src/app/home/home-followed/home-followed.component.spec.ts
+++ src/app/home/home-followed/home-followed.component.spec.ts
@@ -1,6 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { FollowedComponent } from './followed.component';
+import { FollowedComponent } from './home-followed.component';
 
 describe('FollowedComponent', () => {
   let component: FollowedComponent;
