eadbc69fe071a77623d3d5d5147446b238ae7b8c
Osmaldy Maldonado <osmaldy11checo@gmail.com>
2023-05-02 11:47:38 -0400

    Creating component all of search

---
+++ src/app/search/search-all/search-all.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-search-all',
+  templateUrl: './search-all.component.html',
+  styleUrls: ['./search-all.component.css'],
+})
+export class SearchAllComponent {}
