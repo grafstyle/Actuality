95d5c59ffed7d6ac882cbc31d4f78b8122c546ee
Osmaldy Maldonado <osmaldy11checo@gmail.com>
2023-05-02 12:05:01 -0400

    Creating new component for you in home

---
+++ src/app/home/home-for-you/home-for-you.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home-for-you',
+  templateUrl: './home-for-you.component.html',
+  styleUrls: ['./home-for-you.component.css'],
+})
+export class HomeForYouComponent {}
