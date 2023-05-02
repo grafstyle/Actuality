1965f87d573432d232a2d8f801163d3625981817
Osmaldy Maldonado <osmaldy11checo@gmail.com>
2023-05-02 17:14:08 -0400

    Adding all imports/routes of home and search

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,30 @@ import { HomeComponent } from './home/home.component';
 import { ProfileComponent } from './profile/profile.component';
 import { HomeFollowedComponent } from './home/home-followed/home-followed.component';
 import { SearchComponent } from './search/search.component';
+import { HomeForYouComponent } from './home/home-for-you/home-for-you.component';
+import { SearchPostsComponent } from './search/search-posts/search-posts.component';
+import { SearchUsersComponent } from './search/search-users/search-users.component';
+import { SearchAllComponent } from './search/search-all/search-all.component';
 
 const routes: Routes = [
-  { path: '', component: HomeComponent },
-  { path: 'followed', component: HomeFollowedComponent },
+  {
+    path: '',
+    component: HomeComponent,
+    children: [
+      { path: '', component: HomeForYouComponent },
+      { path: 'followed', component: HomeFollowedComponent },
+    ],
+  },
   { path: 'profile', component: ProfileComponent },
-  { path: 'search', component: SearchComponent },
+  {
+    path: 'search',
+    component: SearchComponent,
+    children: [
+      { path: '', component: SearchAllComponent },
+      { path: 'users', component: SearchUsersComponent },
+      { path: 'posts', component: SearchPostsComponent },
+    ],
+  },
   { path: 'home', redirectTo: '' },
 ];
 
