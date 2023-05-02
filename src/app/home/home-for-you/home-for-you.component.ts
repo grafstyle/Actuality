50240e8653b8dde57bfb4e95d5efec0968e4c608
Osmaldy Maldonado <osmaldy11checo@gmail.com>
2023-05-02 13:52:38 -0400

    Moving code of home to for you

--- src/app/home/home-for-you/home-for-you.component.ts
+++ src/app/home/home-for-you/home-for-you.component.ts
@@ -1,8 +1,20 @@
 import { Component } from '@angular/core';
+import { CPost, Posts } from 'src/app/controller/posts/posts';
 
 @Component({
   selector: 'app-home-for-you',
   templateUrl: './home-for-you.component.html',
   styleUrls: ['./home-for-you.component.css'],
 })
-export class HomeForYouComponent {}
+export class HomeForYouComponent {
+  err: string = '';
+  cposts: CPost[] = [];
+
+  async ngOnInit() {
+    try {
+      this.cposts = await Posts.getCPosts(await Posts.getAll());
+    } catch (e) {
+      this.err = 'Something went wrong when get the data.';
+    }
+  }
+}
