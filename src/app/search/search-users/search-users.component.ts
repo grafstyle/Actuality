02eecc62862aefac847ccf99d23c65029fab233c
Osmaldy Maldonado <osmaldy11checo@gmail.com>
2023-05-03 11:23:51 -0400

    Creating layout and getting data to set

--- src/app/search/search-users/search-users.component.ts
+++ src/app/search/search-users/search-users.component.ts
@@ -1,8 +1,32 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { User } from '@auth0/auth0-angular';
+import { Users } from 'src/app/controller/users/users';
 
 @Component({
   selector: 'app-search-users',
   templateUrl: './search-users.component.html',
   styleUrls: ['./search-users.component.css'],
 })
-export class SearchUsersComponent {}
+export class SearchUsersComponent {
+  err: string = '';
+  users: User[] = [];
+
+  constructor(private router: ActivatedRoute) {}
+
+  async ngOnInit(): Promise<void> {
+    try {
+      this.router.queryParams.subscribe({
+        next: async (param) => {
+          if (param['by'] != undefined)
+            this.users = await Users.getIfContains({
+              name: param['by'],
+              url_name: param['by'],
+            });
+        },
+      });
+    } catch (e) {
+      this.err = 'Somthing went wrong when get the users.';
+    }
+  }
+}
