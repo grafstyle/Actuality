25eca9625172552dbceddb751a54d3cdafdcdf89
Osmaldy Maldonado <osmaldy11checo@gmail.com>
2023-05-03 22:44:50 -0400

    Creating layout and getting data to set in search-posts

--- src/app/search/search-posts/search-posts.component.ts
+++ src/app/search/search-posts/search-posts.component.ts
@@ -1,8 +1,28 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { CPost, Posts } from 'src/app/controller/posts/posts';
 
 @Component({
   selector: 'app-search-posts',
   templateUrl: './search-posts.component.html',
   styleUrls: ['./search-posts.component.css'],
 })
-export class SearchPostsComponent {}
+export class SearchPostsComponent {
+  err: string = '';
+  cposts: CPost[] = [];
+
+  constructor(private router: ActivatedRoute) {}
+
+  async ngOnInit() {
+    try {
+      this.router.queryParams.subscribe({
+        next: async (param) => {
+          if (param['by'] != undefined)
+            this.cposts = await Posts.getIfContains(param['by']);
+        },
+      });
+    } catch (e) {
+      this.err = 'Something went wrong when get the posts.';
+    }
+  }
+}
