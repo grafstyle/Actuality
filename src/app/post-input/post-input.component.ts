66e88ba2d35e0a1d9c4724ef70ce18d3166c4e19
Osmaldy Maldonado <osmaldy11checo@gmail.com>
2023-05-04 17:28:30 -0400

    Adding necessary methods of html

--- src/app/post-input/post-input.component.ts
+++ src/app/post-input/post-input.component.ts
@@ -1,8 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { Post } from '../controller/posts/posts';
 
 @Component({
   selector: 'app-post-input',
   templateUrl: './post-input.component.html',
   styleUrls: ['./post-input.component.css'],
 })
-export class PostInputComponent {}
+export class PostInputComponent {
+  toPost: Post = {} as Post;
+  imgs: string[] = [];
+
+  @ViewChild('post_body') postBody!: ElementRef<HTMLDivElement>;
+  @ViewChild('addImgCont') addImgCont!: ElementRef<HTMLDivElement>;
+
+  getBodyText(): string {
+    return this.postBody.nativeElement.innerText;
+  }
+
+  chooseImg(show: boolean): void {
+    if (show) {
+      this.addImgCont.nativeElement.style.opacity = '1';
+      this.addImgCont.nativeElement.style.pointerEvents = 'all';
+      return;
+    }
+
+    this.addImgCont.nativeElement.style.opacity = '0';
+    this.addImgCont.nativeElement.style.pointerEvents = 'none';
+  }
+
+  post(): void {
+    this.toPost.title = this.getBodyText();
+    this.toPost.images = this.imgs;
+    alert();
+  }
+}
