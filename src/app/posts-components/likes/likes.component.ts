7faa85096962b64d79ff2121db0261ac6527e2b9
Osmaldy Maldonado <osmaldy11checo@gmail.com>
2023-05-15 13:52:03 -0400

    Creating methods/styles/callings to likes component

--- src/app/posts-components/likes/likes.component.ts
+++ src/app/posts-components/likes/likes.component.ts
@@ -1,8 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-likes',
   templateUrl: './likes.component.html',
   styleUrls: ['./likes.component.css'],
 })
-export class LikesComponent {}
+export class LikesComponent {
+  @Input() dateAdded: string = '21/08/2005';
+  @Input() cantLikes: number = 0;
+  @Input() idPost: number = 0;
+
+  touchLike: number = 0;
+
+  setLike(): void {
+    this.touchLike++;
+
+    if (this.touchLike == 1) this.addLike();
+
+    if (this.touchLike == 2) {
+      this.touchLike = 0;
+      this.quitLike();
+    }
+  }
+
+  addLike(): void {
+    // Hi i'm not empty and eslint is stupid >:).
+  }
+
+  quitLike(): void {
+    // Hi i'm not empty and eslint is stupid >:).
+  }
+}
