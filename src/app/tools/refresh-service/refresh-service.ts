409b6c0185472b36f46686c48f63c81a8ec5558c
Osmaldy Maldonado <osmaldy11checo@gmail.com>
2023-05-15 01:19:20 -0400

    Creating the respective methods of refresh service

--- src/app/tools/refresh-service/refresh-service.ts
+++ src/app/tools/refresh-service/refresh-service.ts
@@ -1,1 +1,19 @@
-export class RefreshService {}
+import { Injectable } from '@angular/core';
+import { Observable, Subject } from 'rxjs';
+
+@Injectable({ providedIn: 'root' })
+export class RefreshService {
+  private subject: Subject<RefreshData> = new Subject<RefreshData>();
+
+  setUpdate(msg: string): void {
+    this.subject.next({ text: msg });
+  }
+
+  getUpdate(): Observable<RefreshData> {
+    return this.subject.asObservable();
+  }
+}
+
+interface RefreshData {
+  text: string;
+}
