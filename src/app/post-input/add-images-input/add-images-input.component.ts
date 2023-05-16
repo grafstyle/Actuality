65330d40eb6d2edd1270e71dfb001341414eeb87
Osmaldy Maldonado <osmaldy11checo@gmail.com>
2023-05-16 07:52:51 -0400

    Creating add-images-input component

---
+++ src/app/post-input/add-images-input/add-images-input.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-images-input',
+  templateUrl: './add-images-input.component.html',
+  styleUrls: ['./add-images-input.component.css'],
+})
+export class AddImagesInputComponent {}
