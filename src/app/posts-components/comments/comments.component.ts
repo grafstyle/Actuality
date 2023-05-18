415b8883eb33895b9fc633afa8d8e42a62727008
Osmaldy Maldonado <osmaldy11checo@gmail.com>
2023-05-17 21:33:54 -0400

    Migrating html/styles/methods to comments component

--- src/app/posts-components/comments/comments.component.ts
+++ src/app/posts-components/comments/comments.component.ts
@@ -1,8 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { User } from 'src/app/controller/users/users';
+import { Post } from 'src/app/controller/posts/posts';
+import { Comment } from 'src/app/controller/comments/comments';
 
 @Component({
   selector: 'app-comments',
   templateUrl: './comments.component.html',
   styleUrls: ['./comments.component.css'],
 })
-export class CommentsComponent {}
+export class CommentsComponent {
+  @Input() idPost: number = 0;
+  @Input() comments: Comment[] = [];
+  @Input() post: Post = {} as Post;
+  @Input() userOfComments: User = {} as User;
+}
