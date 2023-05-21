85378509451ee19f33fd4868df4de715a7244e02
Osmaldy Maldonado <osmaldy11checo@gmail.com>
2023-05-21 16:01:29 -0400

    Updating all methods referencing to static

--- src/app/controller/comments/comments.ts
+++ src/app/controller/comments/comments.ts
@@ -7,7 +7,7 @@ export class Comments {
 
   public static get(id: number): Promise<Comment[]> {
     return new Promise((res, rej) => {
-      this.apiService.get(`${this.path}/${id}`).subscribe({
+      this.apiService.get(`${Comments.path}/${id}`).subscribe({
         next: (e: any) => res(e as Comment[]),
         error: () => rej([]),
       });
@@ -16,7 +16,7 @@ export class Comments {
 
   public static getAll(): Promise<Comment[]> {
     return new Promise((res, rej) => {
-      this.apiService.get(this.path).subscribe({
+      this.apiService.get(Comments.path).subscribe({
         next: (e: any) => res(e as Comment[]),
         error: () => rej([]),
       });
@@ -46,7 +46,7 @@ export class Comments {
 
   public static post(data: Comment): Promise<string> {
     return new Promise((res, rej) => {
-      this.apiService.post(this.path, data).subscribe({
+      this.apiService.post(Comments.path, data).subscribe({
         next: () => res('The data has been posted.'),
         error: () => rej('Something went wrong when post the data.'),
       });
@@ -55,7 +55,7 @@ export class Comments {
 
   public static put(id: number, data: Comment): Promise<string> {
     return new Promise((res, rej) => {
-      this.apiService.put(this.path, id, data).subscribe({
+      this.apiService.put(Comments.path, id, data).subscribe({
         next: () => res('The data has been updated.'),
         error: () => rej('Something went wrong when update the data.'),
       });
@@ -64,7 +64,7 @@ export class Comments {
 
   public static delete(id: number): Promise<string> {
     return new Promise((res, rej) => {
-      this.apiService.delete(this.path, id).subscribe({
+      this.apiService.delete(Comments.path, id).subscribe({
         next: () => res('The data has been deleted.'),
         error: () => rej('Something went wrong when delete the data.'),
       });
