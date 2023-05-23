2dfd93b1907b401125bdd5aa174edb1972bbaff6
Osmaldy Maldonado <osmaldy11checo@gmail.com>
2023-05-23 16:14:56 -0400

    Creating delete method and updating variable in cloudinary

--- src/app/controller/cloudinary/cloudinary.ts
+++ src/app/controller/cloudinary/cloudinary.ts
@@ -1,14 +1,23 @@
 import { Service } from '../services/services';
 
 export class Cloudinary {
-  private static path: string = 'new/image';
+  private static post_path: string = 'new/file';
   public static apiService: Service;
 
   public static post(data: ImageData): Promise<string> {
     return new Promise((res, rej) => {
-      this.apiService.post(this.path, data).subscribe({
+      this.apiService.post(this.post_path, data).subscribe({
         next: (cloudRes: any) => res(JSON.stringify(cloudRes)),
         error: () => rej('Something went wrong when post the data.'),
+      });
+    });
+  }
+
+  public static delete(url: string): Promise<string> {
+    return new Promise((res, rej) => {
+      this.apiService.deleteFile(url).subscribe({
+        next: (cloudRes: any) => res(JSON.stringify(cloudRes)),
+        error: () => rej('Something went wrong when delete the data.'),
       });
     });
   }
