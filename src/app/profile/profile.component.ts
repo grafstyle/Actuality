531b09315853ab7a9e62db0bdb4af449e39813c8
Osmaldy Maldonado <osmaldy11checo@gmail.com>
2023-06-05 20:34:51 -0400

    User can edit own posts and updating/adding more css/template

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,11 +1,19 @@
-import { Component, ElementRef, ViewChild } from '@angular/core';
+import {
+  ChangeDetectorRef,
+  Component,
+  ElementRef,
+  QueryList,
+  ViewChild,
+  ViewChildren,
+} from '@angular/core';
 import { User, Users } from '../controller/users/users';
-import { CPost, Posts } from '../controller/posts/posts';
+import { CPost, Post, Posts } from '../controller/posts/posts';
 import { Cookies } from '../cookies/cookies';
 import { ActivatedRoute, NavigationEnd, Router } from '@angular/router';
 import { Location } from '@angular/common';
 import { Tools } from '../tools/tools';
 import { Cloudinary } from '../controller/cloudinary/cloudinary';
+import { AddImagesInputComponent } from '../post-input/add-images-input/add-images-input.component';
 
 @Component({
   selector: 'app-profile',
@@ -58,10 +66,28 @@ export class ProfileComponent {
   def_person_img: string =
     'https://res.cloudinary.com/dp5gpr5sc/image/upload/v1685629395/app_assets/person.svg';
 
+  @ViewChildren('edit_post') edit_post!: QueryList<ElementRef<HTMLDivElement>>;
+  @ViewChildren('title_posts') title_posts!: QueryList<
+    ElementRef<HTMLDivElement>
+  >;
+  @ViewChildren('edit_post_imgs_btn') edit_post_imgs_btn!: QueryList<
+    ElementRef<HTMLButtonElement>
+  >;
+  @ViewChild('edit_image_actions')
+  edit_image_actions!: ElementRef<HTMLDivElement>;
+  @ViewChild('imgs_input') add_images_input!: AddImagesInputComponent;
+
+  postToEdit: Post = {} as Post;
+  selectedPost: number = 0;
+
+  lastTextPostTitle: string = '';
+  showImgsInputPost: boolean = false;
+
   constructor(
     private router: ActivatedRoute,
     private location: Location,
-    private routerActions: Router
+    private routerActions: Router,
+    private cd: ChangeDetectorRef
   ) {
     routerActions.events.subscribe({
       next: (e) => {
@@ -310,6 +336,117 @@ export class ProfileComponent {
       this.edit_profile.nativeElement.style.opacity = '0';
       this.edit_profile.nativeElement.style.pointerEvents = 'none';
     }
+  }
+
+  async modalEditPost(
+    open: boolean,
+    elemPos: number,
+    cpostInfo: Post = {} as Post
+  ): Promise<void> {
+    this.selectedPost = elemPos;
+    const elem: HTMLDivElement =
+      this.edit_post.toArray()[elemPos].nativeElement;
+
+    if (open && cpostInfo.id > 0) {
+      elem.style.opacity = '1';
+      elem.style.pointerEvents = 'all';
+
+      this.postToEdit = cpostInfo;
+      return;
+    }
+
+    await this.editPost(elemPos);
+
+    elem.style.opacity = '0';
+    elem.style.pointerEvents = 'none';
+    this.quitNewImgsOfPost();
+  }
+
+  async editPost(elemPos: number): Promise<boolean> {
+    const title = this.title_posts.toArray()[elemPos].nativeElement;
+    const editedPost: Post = {} as Post;
+    const imgsToDB: string[] = [];
+    let somethingEdited: boolean = false;
+
+    if (title.innerText == this.postToEdit.title || title.innerText == '')
+      title.innerText = this.postToEdit.title;
+    else {
+      editedPost.title = title.innerText;
+      somethingEdited = true;
+    }
+
+    if (this.add_images_input != undefined) {
+      for (const url of this.add_images_input.imgsToDelete)
+        Cloudinary.delete(url)
+          .then(() => (somethingEdited = true))
+          .catch(() => alert('Something went wrong at delete some image.'));
+
+      for (const img of this.add_images_input.imgs) {
+        if (img.url.includes('https://res.cloudinary.com'))
+          imgsToDB.push(img.url);
+
+        try {
+          const uploadImage = await Cloudinary.post({
+            name: img.name,
+            image: await this.tools.getImage(img.file),
+            url: `posts/${this.postToEdit.id}/`,
+          });
+
+          imgsToDB.push(await JSON.parse(uploadImage)['secure_url']);
+
+          somethingEdited = true;
+        } catch (err) {
+          /* Catch not empty, or yes? */
+        }
+      }
+
+      if (this.add_images_input.imgsToDelete.length > 0) somethingEdited = true;
+    }
+
+    editedPost.images = imgsToDB;
+
+    if (somethingEdited) {
+      this.postToEdit.date_modified = this.tools.getFormattedActualDate();
+      Posts.put(this.postToEdit.id, editedPost).then(() => this.refreshPosts());
+    }
+
+    return somethingEdited;
+  }
+
+  editImagesOfPost(): void {
+    this.showImgsInputPost = true;
+    this.cd.detectChanges(); // To avoid errors in develop mode.
+
+    const editImgsBtn: HTMLButtonElement =
+      this.edit_post_imgs_btn.toArray()[this.selectedPost].nativeElement;
+    const elem: HTMLDivElement = this.edit_image_actions.nativeElement;
+
+    editImgsBtn.style.opacity = '0';
+    editImgsBtn.style.pointerEvents = 'none';
+
+    if (elem.style.opacity == '0') {
+      elem.style.opacity = '1';
+      elem.style.pointerEvents = 'all';
+      elem.style.height = 'auto';
+    }
+  }
+
+  quitNewImgsOfPost(onlyHide: boolean = false): void {
+    const editImgsBtn: HTMLButtonElement =
+      this.edit_post_imgs_btn.toArray()[this.selectedPost].nativeElement;
+    editImgsBtn.style.opacity = '1';
+    editImgsBtn.style.pointerEvents = 'all';
+
+    if (onlyHide) {
+      const elem: HTMLDivElement = this.edit_image_actions.nativeElement;
+      elem.style.opacity = '0';
+      elem.style.pointerEvents = 'none';
+      elem.style.height = '0';
+      return;
+    }
+
+    this.showImgsInputPost = false;
+    this.cd.detectChanges(); // To avoid errors in develop mode.
   }
 
   deletePost(id: number): void {
