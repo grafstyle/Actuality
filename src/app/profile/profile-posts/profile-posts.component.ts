9633b763bf484b543f77c92164c1c3b115696488
Osmaldy Maldonado <osmaldy11checo@gmail.com>
2023-06-15 18:30:35 -0400

    Creating profile-post component

---
+++ src/app/profile/profile-posts/profile-posts.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-profile-posts',
+  templateUrl: './profile-posts.component.html',
+  styleUrls: ['./profile-posts.component.css'],
+})
+export class ProfilePostsComponent {}
