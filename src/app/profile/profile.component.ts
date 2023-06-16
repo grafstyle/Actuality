457b54769246ca5c7441c0945e736b11d097be1f
Osmaldy Maldonado <osmaldy11checo@gmail.com>
2023-06-16 13:04:18 -0400

    Migrating template of post to profile-post component

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -12,7 +12,7 @@ import { Cookies } from '../cookies/cookies';
 import { ActivatedRoute, NavigationEnd, Router } from '@angular/router';
 import { Tools } from '../tools/tools';
 import { Cloudinary } from '../controller/cloudinary/cloudinary';
-import { AddImagesInputComponent } from '../post-input/add-images-input/add-images-input.component';
+import { ProfilePostsComponent } from './profile-posts/profile-posts.component';
 
 @Component({
   selector: 'app-profile',
@@ -21,10 +21,8 @@ import { AddImagesInputComponent } from '../post-input/add-images-input/add-imag
 })
 export class ProfileComponent {
   err: string = '';
-  noOnePost: string = '';
   user: User = {} as User;
   userRegistered: User = {} as User;
-  cposts: CPost[] = [];
 
   userImgUpload: Image = {} as Image;
   portraitImgUpload: Image = {} as Image;
@@ -73,23 +71,6 @@ export class ProfileComponent {
   def_person_img: string =
     'https://res.cloudinary.com/dp5gpr5sc/image/upload/v1685629395/app_assets/person.svg';
 
-  @ViewChildren('edit_post') edit_post!: QueryList<ElementRef<HTMLDivElement>>;
-  @ViewChildren('title_posts') title_posts!: QueryList<
-    ElementRef<HTMLDivElement>
-  >;
-  @ViewChildren('edit_post_imgs_btn') edit_post_imgs_btn!: QueryList<
-    ElementRef<HTMLButtonElement>
-  >;
-  @ViewChild('edit_image_actions')
-  edit_image_actions!: ElementRef<HTMLDivElement>;
-  @ViewChild('imgs_input') add_images_input!: AddImagesInputComponent;
-
-  postToEdit: Post = {} as Post;
-  selectedPost: number = 0;
-
-  lastTextPostTitle: string = '';
-  showImgsInputPost: boolean = false;
-
   @ViewChild('followe_rs_d_view') frsd_view!: ElementRef<HTMLDivElement>;
   folls: User[] = [];
   follsEmptyStr: string = '';
@@ -135,21 +116,14 @@ export class ProfileComponent {
           this.err = "This user doesn't exist.";
         return;
       }
-
-      this.refreshPosts();
     } catch (e) {
       this.err = 'Something went wrong getting the user.';
     }
   }
 
-  async refreshPosts(): Promise<void> {
-    if (this.user != undefined) {
-      this.cposts = await Posts.getCPosts(
-        await Posts.getBy('id_user', this.user.id)
-      );
-      if (this.cposts.length == 0)
-        this.noOnePost = "The user don't have posts.";
-    }
+  elemLoaded(component: ProfilePostsComponent) {
+    component.user = this.user;
+    if (this.user.id == Cookies.getUserID()) component.canEditPost = true;
   }
 
   async refreshUserOfUrl(): Promise<void> {
@@ -477,130 +451,6 @@ export class ProfileComponent {
       this.edit_profile.nativeElement.style.opacity = '0';
       this.edit_profile.nativeElement.style.pointerEvents = 'none';
     }
-  }
-
-  async modalEditPost(
-    open: boolean,
-    elemPos: number,
-    cpostInfo: Post = {} as Post
-  ): Promise<void> {
-    this.selectedPost = elemPos;
-    const elem: HTMLDivElement =
-      this.edit_post.toArray()[elemPos].nativeElement;
-
-    if (open && cpostInfo.id > 0) {
-      elem.style.opacity = '1';
-      elem.style.pointerEvents = 'all';
-
-      this.postToEdit = cpostInfo;
-      return;
-    }
-
-    await this.editPost(elemPos);
-
-    elem.style.opacity = '0';
-    elem.style.pointerEvents = 'none';
-    this.quitNewImgsOfPost();
-  }
-
-  async editPost(elemPos: number): Promise<boolean> {
-    const title = this.title_posts.toArray()[elemPos].nativeElement;
-    const editedPost: Post = {} as Post;
-    const imgsToDB: string[] = [];
-    let somethingEdited: boolean = false;
-
-    if (title.innerText == this.postToEdit.title || title.innerText == '')
-      title.innerText = this.postToEdit.title;
-    else {
-      editedPost.title = title.innerText;
-      somethingEdited = true;
-    }
-
-    this.showLoadScreen = true;
-
-    if (this.add_images_input != undefined) {
-      for (const url of this.add_images_input.imgsToDelete)
-        Cloudinary.delete(url)
-          .then(() => (somethingEdited = true))
-          .catch(() => alert('Something went wrong at delete some image.'));
-
-      for (const img of this.add_images_input.imgs) {
-        if (img.url.includes('https://res.cloudinary.com'))
-          imgsToDB.push(img.url);
-
-        try {
-          const uploadImage = await Cloudinary.post({
-            name: img.name,
-            image: await this.tools.getImage(img.file),
-            url: `posts/${this.postToEdit.id}/`,
-          });
-
-          imgsToDB.push(await JSON.parse(uploadImage)['secure_url']);
-
-          somethingEdited = true;
-        } catch (err) {
-          /* Catch not empty, or yes? */
-        }
-      }
-
-      if (this.add_images_input.imgsToDelete.length > 0) somethingEdited = true;
-    }
-
-    editedPost.images = imgsToDB;
-
-    if (somethingEdited) {
-      this.postToEdit.date_modified = this.tools.getActualISODate();
-      await Posts.put(this.postToEdit.id, editedPost).then(() => {
-        this.showLoadScreen = false;
-        this.refreshPosts();
-      });
-    }
-
-    return somethingEdited;
-  }
-
-  editImagesOfPost(): void {
-    this.showImgsInputPost = true;
-    this.cd.detectChanges(); // To avoid errors in develop mode.
-
-    const editImgsBtn: HTMLButtonElement =
-      this.edit_post_imgs_btn.toArray()[this.selectedPost].nativeElement;
-    const elem: HTMLDivElement = this.edit_image_actions.nativeElement;
-
-    editImgsBtn.style.opacity = '0';
-    editImgsBtn.style.pointerEvents = 'none';
-
-    if (elem.style.opacity == '0') {
-      elem.style.opacity = '1';
-      elem.style.pointerEvents = 'all';
-      elem.style.height = 'auto';
-    }
-  }
-
-  quitNewImgsOfPost(onlyHide: boolean = false): void {
-    const editImgsBtn: HTMLButtonElement =
-      this.edit_post_imgs_btn.toArray()[this.selectedPost].nativeElement;
-    editImgsBtn.style.opacity = '1';
-    editImgsBtn.style.pointerEvents = 'all';
-
-    if (onlyHide) {
-      const elem: HTMLDivElement = this.edit_image_actions.nativeElement;
-      elem.style.opacity = '0';
-      elem.style.pointerEvents = 'none';
-      elem.style.height = '0';
-      return;
-    }
-
-    this.showImgsInputPost = false;
-    this.cd.detectChanges(); // To avoid errors in develop mode.
-  }
-
-  deletePost(id: number): void {
-    this.showLoadScreen = true;
-    Posts.delete(id).then(() => {
-      this.showLoadScreen = false;
-      this.refreshPosts();
-    });
   }
 
   async showContFoll(show: boolean = true): Promise<void> {
