72381d10cc374de627db4b2b054434cb401f5ad7
Osmaldy Maldonado <osmaldy11checo@gmail.com>
2023-06-16 13:25:49 -0400

    Creating multimedia-posts component

---
+++ src/app/profile/multimedia-posts/multimedia-posts.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-multimedia-posts',
+  templateUrl: './multimedia-posts.component.html',
+  styleUrls: ['./multimedia-posts.component.css'],
+})
+export class MultimediaPostsComponent {}
