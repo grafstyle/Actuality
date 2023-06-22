201b3526393f7781ce11d2e64523b22f7a8a9310
Osmaldy Maldonado <osmaldy11checo@gmail.com>
2023-06-21 20:52:40 -0400

    Creating component alert-component

---
+++ src/app/components/alert-component/alert-component.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-alert-component',
+  templateUrl: './alert-component.component.html',
+  styleUrls: ['./alert-component.component.css'],
+})
+export class AlertComponentComponent {}
